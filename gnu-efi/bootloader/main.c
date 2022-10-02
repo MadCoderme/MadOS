dc528e44693567af2df04be26fad87d65d295515
MadCoderme <youcanreplylishup@gmail.com>
2022-10-02 17:15:04 +0600

    initial commit

---
+++ gnu-efi/bootloader/main.c
@@ -0,0 +1,80 @@
+#include <efi.h>
+#include <efilib.h>
+#include <elf.h>
+
+
+typedef unsigned long long size_t;
+
+EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable ) {
+
+	EFI_FILE* LoadedFile;
+	
+	EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
+	SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
+
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* FileSystem;
+	SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&FileSystem);
+
+	if(Directory == NULL) {
+		FileSystem->OpenVolume(FileSystem, &Directory);
+	}
+
+	EFI_STATUS status = Directory->Open(Directory, &LoadedFile, Path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
+	if(status != EFI_SUCCESS) {
+		return NULL;
+	}
+
+	return LoadedFile;
+
+}
+
+int memcmp(const void* aptr, const void* bptr, size_t n) {
+	const unsigned char* a = aptr, *b = bptr;
+
+	for(size_t i = 0; i < n; i++) {
+		if(a[i] < b[i]) return -1;
+		else if(a[i] > b[i]) return 1;
+	}
+
+	return 0;
+}
+
+EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+	
+	InitializeLib(ImageHandle, SystemTable);
+	
+	EFI_FILE* Kernel = LoadFile(NULL, L"kernel.elf", ImageHandle, SystemTable);
+
+	if(Kernel == NULL) {
+		Print(L"Not loaded yet \n\r");
+	} else {
+		Print(L"it loaded :P \n\r");
+	}
+
+	Elf64_Ehdr header;
+	{
+		UINTN FileInfoSize;
+		EFI_FILE_INFO* FileInfo;
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, NULL);
+		SystemTable->BootServices->AllocatePool(EfiLoaderData, FileInfoSize, (void**)&FileInfo);
+		Kernel->GetInfo(Kernel, &gEfiFileInfoGuid, &FileInfoSize, (void**)&FileInfo);
+
+		UINTN size = sizeof(header);
+		Kernel->Read(Kernel, &size, &header);
+	}
+
+	if(
+		memcmp(&header.e_ident[EI_MAG0], ELFMAG, SELFMAG) != 0 ||
+		header.e_ident[EI_CLASS] != ELFCLASS64 ||
+		header.e_ident[EI_DATA] != ELFDATA2LSB ||
+		header.e_type != ET_EXEC ||
+		header.e_machine != EM_X86_64 ||
+		header.e_version != EV_CURRENT
+	) {
+		Print(L"But kernel format is bad | ERROR \n\r");
+	} else {
+		Print(L"Kernel header verified | SUCCESS \n\r");
+	}
+
+	return EFI_SUCCESS; // Exit the UEFI application
+}
